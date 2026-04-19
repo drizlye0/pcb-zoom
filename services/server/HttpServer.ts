40c23afbd9177573accf7872b5b84a8e1a7d97a7
drizlye0 <danielkastillo03@gmail.com>
2026-04-19 18:42:03 -0500

    feat: adding HttpServer service

---
+++ services/server/HttpServer.ts
@@ -0,0 +1,47 @@
+import { BridgeServer } from 'react-native-http-bridge-refurbished';
+import { WebRTCProvider } from '../webrtc/types';
+
+const DEV_MODE = true;
+
+export class HttpServer {
+  router: BridgeServer;
+  webrtc: WebRTCProvider;
+  port: number;
+
+  constructor(webrtc: WebRTCProvider, port: number) {
+    this.port = port;
+    this.webrtc = webrtc;
+    this.router = new BridgeServer('http_server', DEV_MODE);
+  }
+
+  private setupRoutes() {
+    this.router.get('/offer', async (_req, res) => {
+      const offer = await this.webrtc.createOffer();
+      res.json({ sdp: offer.sdp, type: offer.type }, 200);
+    });
+
+    this.router.post('/answer', async (req, res) => {
+      const answer: RTCSessionDescriptionInit = JSON.parse(req.data);
+      await this.webrtc.handleAnswer(answer);
+
+      res.json({}, 202);
+    });
+
+    this.router.post('/icecandidates', async (req, res) => {
+      const remoteCandidates: RTCIceCandidateInit[] = JSON.parse(req.data);
+      await this.webrtc.handleIceCandidates(remoteCandidates);
+
+      const candidates = this.webrtc.getIceCandidates();
+      if (candidates != null) {
+        res.json(candidates, 200);
+      }
+
+      res.json({}, 204);
+    });
+  }
+
+  startServer() {
+    this.setupRoutes();
+    this.router.listen(this.port);
+  }
+}
