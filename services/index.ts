e8411f6058f7969ac311ecbd1309d231cf0daffd
drizlye0 <danielkastillo03@gmail.com>
2026-04-19 19:18:25 -0500

    feat: dependency injection

---
+++ services/index.ts
@@ -0,0 +1,10 @@
+import { HttpServer } from "./server/HttpServer";
+import { WebRTCManager } from "./webrtc/WebRTCManager";
+
+// TODO: change to settings config
+const PORT = 8080;
+
+const webrtcManager = new WebRTCManager();
+const httpServer = new HttpServer(webrtcManager, PORT);
+
+export { webrtcManager, httpServer };
