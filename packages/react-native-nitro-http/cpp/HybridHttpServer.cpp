7fffbb9123ffe3710e816265a980db26f6f8551f
drizlye0 <danielkastillo03@gmail.com>
2026-04-28 21:42:27 -0500

    feat: testing json lib support

--- packages/react-native-nitro-http/cpp/HybridHttpServer.cpp
+++ packages/react-native-nitro-http/cpp/HybridHttpServer.cpp
@@ -1,6 +1,6 @@
 #include "HybridHttpServer.hpp"
 #include <android/log.h>
-#include <thread>
+#include <httplib.h>
 
 namespace margelo::nitro::nitrohttp {
 void HybridHttpServer::listen(double port) {
@@ -10,8 +10,30 @@ void HybridHttpServer::listen(double port) {
   _isRunning = true;
 
   _srv.Get("/", [](const httplib::Request&, httplib::Response& res) {
-    __android_log_print(ANDROID_LOG_INFO, "NitroHttp", "/ endpoint");
+    __android_log_print(ANDROID_LOG_INFO, "NitroHttp", "GET /");
     res.set_content("Hello, world from http server written on c++", "text/plain");
+  });
+
+  _srv.Post("/jsontest", [](const httplib::Request& req, httplib::Response& res) {
+    __android_log_print(ANDROID_LOG_INFO, "NitroHttp", "POST /jsontest");
+    try {
+      json payload = json::parse(req.body);
+      Offer offer = payload.get<Offer>();
+      __android_log_print(
+          ANDROID_LOG_INFO, "NitroHttp", "sdp: %s , type: %s", offer.sdp.c_str(), offer.type.c_str()
+      );
+
+      offer.sdp = "sdp from c++ nitro module";
+      offer.type = "type from c++ nitro module";
+
+      json response = offer;
+
+      res.set_content(response.dump(), "application/json");
+    } catch (std::exception e) {
+      __android_log_print(
+          ANDROID_LOG_ERROR, "NitroHttp", "failed to process POST /jsontest endpoint"
+      );
+    }
   });
 
   _serverThread = std::thread([this, port] {
--- packages/react-native-nitro-http/cpp/HybridHttpServer.hpp
+++ packages/react-native-nitro-http/cpp/HybridHttpServer.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 #include "HybridHttpServerSpec.hpp"
+#include "nlohmann/detail/macro_scope.hpp"
+#include <android/log.h>
 #include <atomic>
 #include <httplib.h>
+#include <nlohmann/json.hpp>
 #include <thread>
 
 namespace margelo::nitro::nitrohttp {
+using json = nlohmann::json;
+
+class Offer {
+public:
+  std::string sdp;
+  std::string type;
+};
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Offer, sdp, type);
+
 class HybridHttpServer : public HybridHttpServerSpec {
 public:
   HybridHttpServer() : HybridObject(TAG) {}
