a5f3107a08a68726f95a6ffd5efb842ff8e32042
drizlye0 <danielkastillo03@gmail.com>
2026-05-03 19:43:37 -0500

    feat: remove unused http-brigde-refurbished implementation

--- mobile-app/services/index.ts
+++ mobile-app/services/index.ts
@@ -1,10 +1,5 @@
-import { HttpServer } from './server/HttpServer';
 import { WebRTCManager } from './webrtc/WebRTCManager';
 
-// TODO: change to settings config
-const PORT = 8080;
-
 const webrtcManager = new WebRTCManager();
-const httpServer = new HttpServer(webrtcManager, PORT);
 
-export { webrtcManager, httpServer };
+export { webrtcManager };
