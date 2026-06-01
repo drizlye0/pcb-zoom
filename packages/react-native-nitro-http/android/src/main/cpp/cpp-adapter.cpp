129f45b08bd0a06ed29393681912bcbbf934d229
drizlye0 <danielkastillo03@gmail.com>
2026-06-01 17:56:59 -0500

    feat: adding jni calls

--- packages/react-native-nitro-http/android/src/main/cpp/cpp-adapter.cpp
+++ packages/react-native-nitro-http/android/src/main/cpp/cpp-adapter.cpp
@@ -1,9 +1,21 @@
-#include <jni.h>
-#include <fbjni/fbjni.h>
 #include "NitroHttpOnLoad.hpp"
+#include "ServerCore.hpp"
+#include <fbjni/fbjni.h>
+#include <jni.h>
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, []() {
     margelo::nitro::nitrohttp::registerAllNatives();
   });
+}
+
+extern "C" JNIEXPORT void JNICALL Java_com_margelo_nitro_nitrohttp_ServerForeground_nativeStart(
+    JNIEnv* env, jobject thiz, jint port
+) {
+  margelo::nitro::nitrohttp::Server.listen(port);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_margelo_nitro_nitrohttp_ServerForeground_nativeStop(JNIEnv* env, jobject thiz) {
+  margelo::nitro::nitrohttp::Server.stop();
 }
