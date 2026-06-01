9d20cc3114aadbbd8d9b1665f34644cea610e258
drizlye0 <danielkastillo03@gmail.com>
2026-06-01 17:56:51 -0500

    feat: set callback implementation

--- packages/react-native-nitro-http/cpp/HCallbackManager.cpp
+++ packages/react-native-nitro-http/cpp/HCallbackManager.cpp
@@ -1,9 +1,16 @@
 #include "HCallbackManager.hpp"
+#include "ServerCore.hpp"
 
 namespace margelo::nitro::nitrohttp {
-void HCallbackManager::setOffer(const OfferCallback& cb) {}
+void HCallbackManager::setOffer(const OfferCallback& cb) {
+  Server._offerCb = cb;
+}
 
-void HCallbackManager::setAnswer(const AnswerCallback& cb) {}
+void HCallbackManager::setAnswer(const AnswerCallback& cb) {
+  Server._answerCb = cb;
+}
 
-void HCallbackManager::setIceCandidate(const IceCandidateCallback& cb) {}
+void HCallbackManager::setIceCandidate(const IceCandidateCallback& cb) {
+  Server._candidateCb = cb;
+}
 } // namespace margelo::nitro::nitrohttp
